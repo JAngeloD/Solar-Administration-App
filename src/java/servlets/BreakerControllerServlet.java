7d590805977df4e017fb17edc6fcc8254536ccb6
856622 <856622@SAIT227036IT.ACDM.DS.SAIT.CA>
2022-02-03 17:58:24 -0700

    Polish some forms and remove env variables

---
+++ src/java/servlets/BreakerControllerServlet.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 856622
+ */
+public class BreakerControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //Load up a JSP
+        getServletContext().getRequestDispatcher("/WEB-INF/EXAMPLE.JSP").forward(request,response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      getServletContext().getRequestDispatcher("/WEB-INF/EXAMPLE.jsp").forward(request, response);
+      return;
+    }
+}

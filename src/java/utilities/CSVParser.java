11b527eee5983124872d5115a24a440348ad675f
Andrew Vergara <71391238+Andrew-Vergara@users.noreply.github.com>
2022-02-15 13:23:08 -0700

    Update CSVParser.java

--- src/java/utilities/CSVParser.java
+++ src/java/utilities/CSVParser.java
@@ -45,246 +45,101 @@ public class CSVParser {
       try (CSVReader reader = new CSVReader(new FileReader(fileLocation))){
             String[] lineInArray; 
             while ((lineInArray = reader.readNext()) != null) {
-                if(lineInArray[2].equals("no data")){
-                    facility.setSolarIrridinacePOA(0);
-                }else {
-                    facility.setSolarIrridinacePOA(Double.parseDouble(lineInArray[2]));
-                }
-                if(lineInArray[3].equals("no data")) {
-                    facility.setSolarIrridinaceGHI(0);
-                } else {
-                    facility.setSolarIrridinaceGHI(Double.parseDouble(lineInArray[3]));
-                }
-                if(lineInArray[4].equals("no data")) {
-                    facility.setBackOfPanelTemperature1(0);
-                } else {
-                    facility.setBackOfPanelTemperature1(Double.parseDouble(lineInArray[4]));
-                }
-                if(lineInArray[5].equals("no data")) {
-                    facility.setAmbientTemperature(0);
-                } else {
-                    facility.setAmbientTemperature(Double.parseDouble(lineInArray[5]));
-                }
-                if(lineInArray[6].equals("no data")) {
-                    facility.setBackOfPanelTemperature2(0);
-                } else {
-                    facility.setBackOfPanelTemperature2(Double.parseDouble(lineInArray[6]));
-                }
-                if(lineInArray[7].equals("no data")) {
-                    facility.setWindSpeed(0);
-                } else {
-                    facility.setWindSpeed(Double.parseDouble(lineInArray[7]));
-                }
-                if(lineInArray[8].equals("no data")){
-                    pcc.setAcOutputLifeTimeEnergy(0);
-                } else {
-                    pcc.setAcOutputLifeTimeEnergy(Double.parseDouble(lineInArray[8]));
-                }
-                if(lineInArray[9].equals("no data")){
-                    pcc.setAcOutputYearToDateEnergy(0);
-                } else {
-                    pcc.setAcOutputYearToDateEnergy(Double.parseDouble(lineInArray[9]));
-                }
-                if(lineInArray[10].equals("no data")){
-                    pcc.setAcOutputMonthToDateEnergy(0);
-                } else {
-                    pcc.setAcOutputMonthToDateEnergy(Double.parseDouble(lineInArray[10]));
-                }
-                if(lineInArray[11].equals("no data")){
-                    pcc.setAcOutputDailyEnergy(0);
-                } else {
-                    pcc.setAcOutputDailyEnergy(Double.parseDouble(lineInArray[11]));
-                }
-                if(lineInArray[12].equals("no data")){
-                    pcc.setAcOutputRealPower(0);
-                } else {
-                    pcc.setAcOutputRealPower(Double.parseDouble(lineInArray[12]));
-                }
-                if(lineInArray[13].equals("no data")){
-                    pcc.setAcOutputApparentPower(0);
-                } else {
-                    pcc.setAcOutputApparentPower(Double.parseDouble(lineInArray[13]));
-                }
-                if(lineInArray[14].equals("no data")){
-                    pcc.setAcOutputReactivePower(0);
-                } else {
-                    pcc.setAcOutputReactivePower(Double.parseDouble(lineInArray[14]));
-                }
-                if(lineInArray[15].equals("no data")){
-                    pcc.setAcOutputPowerFactor(0);
-                } else {
-                    pcc.setAcOutputPowerFactor(Double.parseDouble(lineInArray[15]));
-                }
-                if(lineInArray[16].equals("no data")){
-                    pcc.setAcOutputPhaseACurrent(0);
-                } else {
-                    pcc.setAcOutputPhaseACurrent(Double.parseDouble(lineInArray[16]));
-                }
-                if(lineInArray[17].equals("no data")){
-                    pcc.setAcOutputPhaseBCurrent(0);
-                } else {
-                    pcc.setAcOutputPhaseBCurrent(Double.parseDouble(lineInArray[17]));
-                }
-                if(lineInArray[18].equals("no data")){
-                    pcc.setAcOutputPhaseCCurrent(0);
-                } else {
-                    pcc.setAcOutputPhaseCCurrent(Double.parseDouble(lineInArray[18]));
-                }
-                if(lineInArray[19].equals("no data")){
-                    pcc.setAcOutputPhaseABVoltage(0);
-                } else {
-                    pcc.setAcOutputPhaseABVoltage(Double.parseDouble(lineInArray[19]));
-                }
-                if(lineInArray[20].equals("no data")){
-                    pcc.setAcOutputPhaseBCVoltage(0);
-                } else {
-                    pcc.setAcOutputPhaseBCVoltage(Double.parseDouble(lineInArray[20]));
-                }
-                if(lineInArray[21].equals("no data")){
-                    pcc.setAcOutputPhaseCAVoltage(0);
-                } else {
-                    pcc.setAcOutputPhaseCAVoltage(Double.parseDouble(lineInArray[21]));
-                }
-                if(lineInArray[22].equals("no data")) {
-                    feeder.setAcOutputRealPower(0);
-                } else {
-                    feeder.setAcOutputRealPower(Double.parseDouble(lineInArray[22]));
-                }
-                if(lineInArray[23].equals("no data")) {
-                    feeder.setAcOutputApparentPower(0);
-                } else {
-                    feeder.setAcOutputApparentPower(Double.parseDouble(lineInArray[23]));
-                }
-                if(lineInArray[24].equals("no data")) {
-                    feeder.setAcOutputReactivePower (0);
-                } else {
-                    feeder.setAcOutputReactivePower(Double.parseDouble(lineInArray[24]));
-                }
-                if(lineInArray[25].equals("no data")) {
-                    feeder.setAcOutputPowerFactor(0);
-                } else {
-                    feeder.setAcOutputPowerFactor(Double.parseDouble(lineInArray[25]));
-                }
-                if(lineInArray[26].equals("no data")) {
-                    feeder.setAcOutputPhaseACurrent(0);
-                } else {
-                    feeder.setAcOutputPhaseACurrent(Double.parseDouble(lineInArray[26]));
-                }
-                if(lineInArray[27].equals("no data")) {
-                    feeder.setAcOutputPhaseBCurrent(0);
-                } else {
-                    feeder.setAcOutputPhaseBCurrent(Double.parseDouble(lineInArray[27]));
-                }
-                if(lineInArray[28].equals("no data")) {
-                    feeder.setAcOutputPhaseCCurrent(0);
-                } else {
-                    feeder.setAcOutputPhaseCCurrent(Double.parseDouble(lineInArray[28]));
-                }
-                if(lineInArray[29].equals("no data")) {
-                    feeder.setAcOutputPhaseABVoltage(0);
-                } else {
-                    feeder.setAcOutputPhaseABVoltage(Double.parseDouble(lineInArray[29]));
-                }
-                if(lineInArray[30].equals("no data")) {
-                    feeder.setAcOutputPhaseBCVoltage(0);
-                } else {
-                    feeder.setAcOutputPhaseBCVoltage(Double.parseDouble(lineInArray[30]));
-                }
-                if(lineInArray[31].equals("no data")) {
-                    feeder.setAcOutputPhaseCAVoltage(0);
-                } else {
-                    feeder.setAcOutputPhaseCAVoltage(Double.parseDouble(lineInArray[31]));
-                }
-                if(lineInArray[42].equals("no data")) {
-                    inverter.setAcOutputLifeTimeEnergy(0);
-                } else {
-                    inverter.setAcOutputLifeTimeEnergy(Double.parseDouble(lineInArray[42]));
-                }
-                if(lineInArray[43].equals("no data")) {
-                    inverter.setAcOutputYearToDateEnergy(0);
-                } else {
-                    inverter.setAcOutputYearToDateEnergy(Double.parseDouble(lineInArray[43]));
-                }
-                if(lineInArray[44].equals("no data")) {
-                    inverter.setAcOutputMonthToDateEnergy(0);
-                } else {
-                    inverter.setAcOutputMonthToDateEnergy(Double.parseDouble(lineInArray[44]));
-                }
-                if(lineInArray[45].equals("no data")) {
-                    inverter.setAcOutputDailyEnergy(0);
-                } else {
-                    inverter.setAcOutputDailyEnergy(Double.parseDouble(lineInArray[45]));
-                }
-                if(lineInArray[46].equals("no data")) {
-                    inverter.setAcOutputRealPower(0);
-                } else {
-                    inverter.setAcOutputRealPower(Double.parseDouble(lineInArray[46]));
-                }
-                if(lineInArray[47].equals("no data")) {
-                    inverter.setAcOutputApparentPower(0);
-                } else {
-                    inverter.setAcOutputApparentPower(Double.parseDouble(lineInArray[47]));
-                }
-                if(lineInArray[48].equals("no data")) {
-                    inverter.setAcOutputReactivePower(0);
-                } else {
-                    inverter.setAcOutputReactivePower(Double.parseDouble(lineInArray[48]));
-                }
-                if(lineInArray[49].equals("no data")) {
-                    inverter.setAcOutputPowerFactor(0);
-                } else {
-                    inverter.setAcOutputPowerFactor(Double.parseDouble(lineInArray[49]));
-                }
-                if(lineInArray[50].equals("no data")) {
-                    inverter.setAcOutputPhaseACurrent(0);
-                } else {
-                    inverter.setAcOutputPhaseACurrent(Double.parseDouble(lineInArray[50]));
-                }
-                if(lineInArray[51].equals("no data")) {
-                    inverter.setAcOutputPhaseBCurrent(0);
-                } else {
-                    inverter.setAcOutputPhaseBCurrent(Double.parseDouble(lineInArray[51]));
-                }
-                if(lineInArray[52].equals("no data")) {
-                    inverter.setAcOutputPhaseCCurrent(0);
-                } else {
-                    inverter.setAcOutputPhaseCCurrent(Double.parseDouble(lineInArray[52]));
-                }
-                if(lineInArray[53].equals("no data")) {
-                    inverter.setAcOutputPhaseABVoltage(0);
-                } else {
-                    inverter.setAcOutputPhaseABVoltage(Double.parseDouble(lineInArray[53]));
-                }
-                if(lineInArray[54].equals("no data")) {
-                    inverter.setAcOutputPhaseBCVoltage(0);
-                } else {
-                    inverter.setAcOutputPhaseBCVoltage(Double.parseDouble(lineInArray[54]));
-                }
-                if(lineInArray[55].equals("no data")) {
-                    inverter.setAcOutputPhaseCAVoltage(0);
-                } else {
-                    inverter.setAcOutputPhaseCAVoltage(Double.parseDouble(lineInArray[55]));
-                }
-                if(lineInArray[56].equals("no data")) {
-                    inverter.setDcRealPower(0);
-                } else {
-                    inverter.setDcRealPower(Double.parseDouble(lineInArray[56]));
-                }
-                if(lineInArray[57].equals("no data")) {
-                    inverter.setDcVolts(0);
-                } else {
-                    inverter.setDcVolts(Double.parseDouble(lineInArray[57]));
-                }
-                if(lineInArray[58].equals("no data")) {
-                    inverter.setDcCurrent(0);
-                } else {
-                    inverter.setDcCurrent(Double.parseDouble(lineInArray[58]));
-                }
-                if(lineInArray[59].equals("no data")) {
-                    inverter.setEfficiency(0);
-                } else {
-                    inverter.setEfficiency(Double.parseDouble(lineInArray[59]));
-                }
+                 facility.setSolarIrridinacePOA(Double.parseDouble(lineInArray[3].replaceAll(",", "")));
+                    
+                    facility.setSolarIrridinaceGHI(Double.parseDouble(lineInArray[4].replaceAll(",", "")));
+
+                    facility.setBackOfPanelTemperature1(Double.parseDouble(lineInArray[5].replaceAll(",", "")));
+                
+                    facility.setAmbientTemperature(Double.parseDouble(lineInArray[6].replaceAll(",", "")));
+              
+                    facility.setBackOfPanelTemperature2(Double.parseDouble(lineInArray[7]));
+                    
+                    facility.setWindSpeed(Double.parseDouble(lineInArray[8].replaceAll(",", "")));
+
+                    pcc.setAcOutputLifeTimeEnergy(Double.parseDouble(lineInArray[9].replaceAll(",", "")));
+
+                    pcc.setAcOutputYearToDateEnergy(Double.parseDouble(lineInArray[10].replaceAll(",", "")));
+
+                    pcc.setAcOutputMonthToDateEnergy(Double.parseDouble(lineInArray[11].replaceAll(",", "")));
+
+                    pcc.setAcOutputDailyEnergy(Double.parseDouble(lineInArray[12].replaceAll(",", "")));
+
+                    pcc.setAcOutputRealPower(Double.parseDouble(lineInArray[13].replaceAll(",", "")));
+
+                    pcc.setAcOutputApparentPower(Double.parseDouble(lineInArray[14].replaceAll(",", "")));
+
+                    pcc.setAcOutputReactivePower(Double.parseDouble(lineInArray[15].replaceAll(",", "")));
+
+                    pcc.setAcOutputPowerFactor(Double.parseDouble(lineInArray[16].replaceAll(",", "")));
+
+                    pcc.setAcOutputPhaseACurrent(Double.parseDouble(lineInArray[17].replaceAll(",", "")));
+
+                    pcc.setAcOutputPhaseBCurrent(Double.parseDouble(lineInArray[18].replaceAll(",", "")));
+
+                    pcc.setAcOutputPhaseCCurrent(Double.parseDouble(lineInArray[19].replaceAll(",", "")));
+
+                    pcc.setAcOutputPhaseABVoltage(Double.parseDouble(lineInArray[20].replaceAll(",", "")));
+
+                    pcc.setAcOutputPhaseBCVoltage(Double.parseDouble(lineInArray[21].replaceAll(",", "")));
+
+                    pcc.setAcOutputPhaseCAVoltage(Double.parseDouble(lineInArray[22].replaceAll(",", "")));
+
+                    feeder.setAcOutputRealPower(Double.parseDouble(lineInArray[23].replaceAll(",", "")));
+  
+                    feeder.setAcOutputApparentPower(Double.parseDouble(lineInArray[24].replaceAll(",", "")));
+
+                    feeder.setAcOutputReactivePower(Double.parseDouble(lineInArray[25].replaceAll(",", "")));
+
+                    feeder.setAcOutputPowerFactor(Double.parseDouble(lineInArray[26].replaceAll(",", "")));
+
+                    feeder.setAcOutputPhaseACurrent(Double.parseDouble(lineInArray[27].replaceAll(",", "")));
+ 
+                    feeder.setAcOutputPhaseBCurrent(Double.parseDouble(lineInArray[28].replaceAll(",", "")));
+   
+                    feeder.setAcOutputPhaseCCurrent(Double.parseDouble(lineInArray[29].replaceAll(",", "")));
+   
+                    feeder.setAcOutputPhaseABVoltage(Double.parseDouble(lineInArray[30].replaceAll(",", "")));
+
+                    feeder.setAcOutputPhaseBCVoltage(Double.parseDouble(lineInArray[31].replaceAll(",", "")));
+    
+                    feeder.setAcOutputPhaseCAVoltage(Double.parseDouble(lineInArray[32].replaceAll(",", "")));
+
+                    inverter.setAcOutputLifeTimeEnergy(Double.parseDouble(lineInArray[43].replaceAll(",", "")));
+
+                    inverter.setAcOutputYearToDateEnergy(Double.parseDouble(lineInArray[44].replaceAll(",", "")));
+      
+                    inverter.setAcOutputMonthToDateEnergy(Double.parseDouble(lineInArray[45].replaceAll(",", "")));
+ 
+                    inverter.setAcOutputDailyEnergy(Double.parseDouble(lineInArray[46].replaceAll(",", "")));
+    
+                    inverter.setAcOutputRealPower(Double.parseDouble(lineInArray[47].replaceAll(",", "")));
+     
+                    inverter.setAcOutputApparentPower(Double.parseDouble(lineInArray[48].replaceAll(",", "")));
+
+                    inverter.setAcOutputReactivePower(Double.parseDouble(lineInArray[49].replaceAll(",", "")));
+
+                    inverter.setAcOutputPowerFactor(Double.parseDouble(lineInArray[50].replaceAll(",", "")));
+
+                    inverter.setAcOutputPhaseACurrent(Double.parseDouble(lineInArray[51].replaceAll(",", "")));
+
+                    inverter.setAcOutputPhaseBCurrent(Double.parseDouble(lineInArray[52].replaceAll(",", "")));
+
+                    inverter.setAcOutputPhaseCCurrent(Double.parseDouble(lineInArray[53].replaceAll(",", "")));
+
+                    inverter.setAcOutputPhaseABVoltage(Double.parseDouble(lineInArray[54].replaceAll(",", "")));
+   
+                    inverter.setAcOutputPhaseBCVoltage(Double.parseDouble(lineInArray[55].replaceAll(",", "")));
+
+                    inverter.setAcOutputPhaseCAVoltage(Double.parseDouble(lineInArray[56].replaceAll(",", "")));
+    
+                    inverter.setDcRealPower(Double.parseDouble(lineInArray[57].replaceAll(",", "")));
+
+                    inverter.setDcVolts(Double.parseDouble(lineInArray[58].replaceAll(",", "")));
+
+                    inverter.setDcCurrent(Double.parseDouble(lineInArray[59].replaceAll(",", "")));
+
+                    inverter.setEfficiency(Double.parseDouble(lineInArray[60].replaceAll(",", "")));
                    
             }
         }

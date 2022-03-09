67123c5e2e21cb9f452412953ec7e314c432bdf6
821320 <821320@SAIT227654IT.ACDM.DS.SAIT.CA>
2022-03-08 16:45:50 -0800

    DB utils completed..? All Models have a create, read, read all, update, and delete function.

---
+++ src/java/dbutil/BreakersDB.java
@@ -0,0 +1,93 @@
+
+package dbutil;
+
+import java.sql.SQLException;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import models.Breakers;
+
+/**
+ *
+ * @author 821320
+ */
+public class BreakersDB {
+        List<Breakers> breakerList;
+
+    public List<Breakers> getAll() throws SQLException {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            breakerList = em.createNamedQuery("User.findAll", Breakers.class).getResultList();
+        } finally {
+            em.close();
+        }
+
+        return breakerList;
+    }
+
+    public Breakers get(String component) {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        Breakers user = null;
+        try {
+            user = em.find(Breakers.class, component);
+        } finally {
+            em.close();
+        }
+
+        return user;
+    }
+
+    public void insert(Breakers user) throws SQLException {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        try {
+            trans.begin();
+            em.persist(user);
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public void update(Breakers user) {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+            trans.begin();
+            em.merge(user);
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public void delete(String component) throws SQLException {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        //delete user
+        try {
+            trans.begin();
+            em.remove(em.find(Breakers.class, component));
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+}
---
+++ src/java/dbutil/TimestampDB.java
@@ -0,0 +1,93 @@
+
+package dbutil;
+
+import java.sql.SQLException;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import models.TimestampDesc;
+
+/**
+ *
+ * @author 821320
+ */
+public class TimestampDB {
+    List<TimestampDesc> timeList;
+
+    public List<TimestampDesc> getAll() throws SQLException {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            timeList = em.createNamedQuery("User.findAll", TimestampDesc.class).getResultList();
+        } finally {
+            em.close();
+        }
+
+        return timeList;
+    }
+
+    public TimestampDesc get(String component) {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        TimestampDesc user = null;
+        try {
+            user = em.find(TimestampDesc.class, component);
+        } finally {
+            em.close();
+        }
+
+        return user;
+    }
+
+    public void insert(TimestampDesc user) throws SQLException {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        try {
+            trans.begin();
+            em.persist(user);
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public void update(TimestampDesc user) {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+            trans.begin();
+            em.merge(user);
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public void delete(String component) throws SQLException {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        //delete user
+        try {
+            trans.begin();
+            em.remove(em.find(TimestampDesc.class, component));
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+}

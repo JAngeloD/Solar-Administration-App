6ef11bd107305876f8f6677452a50b45fb3420b8
Hazbleydy Cortes <90346935+hazac@users.noreply.github.com>
2022-03-23 16:28:02 -0600

    Updated reports.jsp, created repStyle.css and reports.js

---
+++ web/javascript/reports.js
@@ -0,0 +1,358 @@
+function displayPCC(){
+    document.getElementById("pccBlock").style.display="block";
+}
+
+function closePCC(){
+    document.getElementById("pccBlock").style.display="none";
+}
+
+function displayFeeders(){
+    document.getElementById("fdrs").style.display="block";
+}
+
+function closeFeeders(){
+    document.getElementById("fdrs").style.display="none";
+}
+
+function displayFeeder1(){
+    document.getElementById("fdr1").style.display="block";
+}
+
+function closeFeeder1(){
+    document.getElementById("fdr1").style.display="none";
+}
+
+function displayFeeder2(){
+    document.getElementById("fdr2").style.display="block";
+}
+
+function closeFeeder2(){
+    document.getElementById("fdr2").style.display="none";
+}
+
+function displayInverters(){
+    document.getElementById("invtrs").style.display="block";
+}
+
+function closeInverters(){
+    document.getElementById("invtrs").style.display="none";
+}
+
+function displayInverter1(){
+    document.getElementById("inv1").style.display="block";
+}
+
+function closeInverter1(){
+    document.getElementById("inv1").style.display="none";
+}
+
+function displayInverter2(){
+    document.getElementById("inv2").style.display="block";
+}
+
+function closeInverter2(){
+    document.getElementById("inv2").style.display="none";
+}
+
+function displayInverter3(){
+    document.getElementById("inv3").style.display="block";
+}
+
+function closeInverter3(){
+    document.getElementById("inv3").style.display="none";
+}
+
+function displayInverter4(){
+    document.getElementById("inv4").style.display="block";
+}
+
+function closeInverter4(){
+    document.getElementById("inv4").style.display="none";
+}
+
+function displayInverter5(){
+    document.getElementById("inv5").style.display="block";
+}
+
+function closeInverter5(){
+    document.getElementById("inv5").style.display="none";
+}
+
+function displayInverter6(){
+    document.getElementById("inv6").style.display="block";
+}
+
+function closeInverter6(){
+    document.getElementById("inv6").style.display="none";
+}
+
+function displayInverter7(){
+    document.getElementById("inv7").style.display="block";
+}
+
+function closeInverter7(){
+    document.getElementById("inv7").style.display="none";
+}
+
+function displayInverter8(){
+    document.getElementById("inv8").style.display="block";
+}
+
+function closeInverter8(){
+    document.getElementById("inv8").style.display="none";
+}
+
+function displayInverter9(){
+    document.getElementById("inv9").style.display="block";
+}
+
+function closeInverter9(){
+    document.getElementById("inv9").style.display="none";
+}
+
+function displayInverter10(){
+    document.getElementById("inv10").style.display="block";
+}
+
+function closeInverter10(){
+    document.getElementById("inv10").style.display="none";
+}
+
+function displayInverter11(){
+    document.getElementById("inv11").style.display="block";
+}
+
+function closeInverter11(){
+    document.getElementById("inv11").style.display="none";
+}
+
+function displayInverter12(){
+    document.getElementById("inv12").style.display="block";
+}
+
+function closeInverter12(){
+    document.getElementById("inv12").style.display="none";
+}
+
+function displayInverter13(){
+    document.getElementById("inv13").style.display="block";
+}
+
+function closeInverter13(){
+    document.getElementById("inv13").style.display="none";
+}
+
+function displayInverter14(){
+    document.getElementById("inv14").style.display="block";
+}
+
+function closeInverter14(){
+    document.getElementById("inv14").style.display="none";
+}
+
+function displayInverter15(){
+    document.getElementById("inv15").style.display="block";
+}
+
+function closeInverter15(){
+    document.getElementById("inv15").style.display="none";
+}
+
+function displayInverter16(){
+    document.getElementById("inv16").style.display="block";
+}
+
+function closeInverter16(){
+    document.getElementById("inv16").style.display="none";
+}
+
+function displayInverter17(){
+    document.getElementById("inv17").style.display="block";
+}
+
+function closeInverter17(){
+    document.getElementById("inv17").style.display="none";
+}
+
+function displayInverter18(){
+    document.getElementById("inv18").style.display="block";
+}
+
+function closeInverter18(){
+    document.getElementById("inv18").style.display="none";
+}
+
+function displayInverter19(){
+    document.getElementById("inv19").style.display="block";
+}
+
+function closeInverter19(){
+    document.getElementById("inv19").style.display="none";
+}
+
+function displayInverter20(){
+    document.getElementById("inv20").style.display="block";
+}
+
+function closeInverter20(){
+    document.getElementById("inv20").style.display="none";
+}
+
+function displayInverter21(){
+    document.getElementById("inv21").style.display="block";
+}
+
+function closeInverter21(){
+    document.getElementById("inv21").style.display="none";
+}
+
+function displayInverter22(){
+    document.getElementById("inv22").style.display="block";
+}
+
+function closeInverter22(){
+    document.getElementById("inv22").style.display="none";
+}
+
+function displayInverter23(){
+    document.getElementById("inv23").style.display="block";
+}
+
+function closeInverter23(){
+    document.getElementById("inv23").style.display="none";
+}
+
+function displayInverter24(){
+    document.getElementById("inv24").style.display="block";
+}
+
+function closeInverter24(){
+    document.getElementById("inv24").style.display="none";
+}
+
+function displayInverter25(){
+    document.getElementById("inv25").style.display="block";
+}
+
+function closeInverter25(){
+    document.getElementById("inv25").style.display="none";
+}
+
+function displayInverter26(){
+    document.getElementById("inv26").style.display="block";
+}
+
+function closeInverter26(){
+    document.getElementById("inv26").style.display="none";
+}
+
+function displayInverter27(){
+    document.getElementById("inv27").style.display="block";
+}
+
+function closeInverter27(){
+    document.getElementById("inv27").style.display="none";
+}
+
+function displayInverter28(){
+    document.getElementById("inv28").style.display="block";
+}
+
+function closeInverter28(){
+    document.getElementById("inv28").style.display="none";
+}
+
+function displayInverter29(){
+    document.getElementById("inv29").style.display="block";
+}
+
+function closeInverter29(){
+    document.getElementById("inv29").style.display="none";
+}
+
+function displayInverter30(){
+    document.getElementById("inv30").style.display="block";
+}
+
+function closeInverter30(){
+    document.getElementById("inv30").style.display="none";
+}
+
+function displayInverter31(){
+    document.getElementById("inv31").style.display="block";
+}
+
+function closeInverter31(){
+    document.getElementById("inv31").style.display="none";
+}
+
+function displayInverter32(){
+    document.getElementById("inv32").style.display="block";
+}
+
+function closeInverter32(){
+    document.getElementById("inv32").style.display="none";
+}
+function displayInverter33(){
+    document.getElementById("inv33").style.display="block";
+}
+
+function closeInverter33(){
+    document.getElementById("inv33").style.display="none";
+}
+
+function displayInverter34(){
+    document.getElementById("inv34").style.display="block";
+}
+
+function closeInverter34(){
+    document.getElementById("inv34").style.display="none";
+}
+
+function displayInverter35(){
+    document.getElementById("inv35").style.display="block";
+}
+
+function closeInverter35(){
+    document.getElementById("inv35").style.display="none";
+}
+
+function displayInverter36(){
+    document.getElementById("inv36").style.display="block";
+}
+
+function closeInverter36(){
+    document.getElementById("inv36").style.display="none";
+}
+
+function displayInverter37(){
+    document.getElementById("inv37").style.display="block";
+}
+
+function closeInverter37(){
+    document.getElementById("inv37").style.display="none";
+}
+
+function displayInverter38(){
+    document.getElementById("inv38").style.display="block";
+}
+
+function closeInverter38(){
+    document.getElementById("inv38").style.display="none";
+}
+
+function displayInverter39(){
+    document.getElementById("inv39").style.display="block";
+}
+
+function closeInverter39(){
+    document.getElementById("inv39").style.display="none";
+}
+
+function displayWeather(){
+    document.getElementById("wthr").style.display="block";
+}
+
+function closeWeather(){
+    document.getElementById("wthr").style.display="none";
+}

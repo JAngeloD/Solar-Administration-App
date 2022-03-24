3eb9a7ef380428439eb3b3df36f3756f7937d532
856622 <856622@SAIT227036IT.ACDM.DS.SAIT.CA>
2022-03-24 08:13:43 -0600

    merge conflict resolve

--- src/java/servlets/ReportServlet.java
+++ src/java/servlets/ReportServlet.java
@@ -20,6 +20,7 @@ public class ReportServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
     }
 
     @Override

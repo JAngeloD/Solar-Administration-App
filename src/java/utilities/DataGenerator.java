876c8ea5e81c1fd5065ea3928e429c1918e92f72
Andrew Vergara <71391238+Andrew-Vergara@users.noreply.github.com>
2022-04-11 03:06:57 -0600

    Update DataGenerator.java

--- src/java/utilities/DataGenerator.java
+++ src/java/utilities/DataGenerator.java
@@ -19,55 +19,123 @@ import models.*;
  */
 public class DataGenerator {
 
-    private String recordID = "100000000";
-    private long timeStampID = 4193;
-    private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
-    private Date timeStamp = new Date(System.currentTimeMillis());
+   
+    private Facility facility = new Facility();
+    private PCC pcc = new PCC();
+    private Inverter inverter = new Inverter();
+    private String recordID = "0";
+    private long timeStampID = 0;
+    private Date timeStamp = new Date();
 
-    public String recordID() {
+    public Facility randomDataFacility() {
         int tempRecord = 0;
-        tempRecord = Integer.parseInt(recordID);
-        tempRecord++;
-
-        return Integer.toString(tempRecord);
-    }
-
-    public Long timeStampId() {
-        return timeStampID++;
-    }
-
-    public Date timeStamp() {
-        return timeStamp;
-    }
-
-    public Double randomPOA() {
+        tempRecord = new Random().nextInt() * 1000000;
+        recordID = Integer.toString(tempRecord);
+        TimeFactory timeFactory = new TimeFactory();
+        timeStampID = timeFactory.getCurrentID();
+        Date date = new Date();
         double randomPOA = new Random().nextDouble() * (1071 - -5) + -5;
-        BigDecimal bd = new BigDecimal(randomPOA).setScale(2, RoundingMode.CEILING);
-        return bd.doubleValue();
-    }
-
-    public double randomGHI() {
+        BigDecimal bd1 = new BigDecimal(randomPOA).setScale(2, RoundingMode.CEILING);
         double randomGHI = new Random().nextDouble() * (3248);
-        BigDecimal bd = new BigDecimal(randomGHI).setScale(2, RoundingMode.CEILING);
-        return bd.doubleValue();
-    }
-
-    public double randomTemp() {
+        BigDecimal bd2 = new BigDecimal(randomGHI).setScale(2, RoundingMode.CEILING);
         double randomTemp = new Random().nextDouble() * (39.3 - -13.6) + -13.6;
-        BigDecimal bd = new BigDecimal(randomTemp).setScale(2, RoundingMode.CEILING);
-        return bd.doubleValue();
-    }
-
-    public double randomAmbientTemp() {
+        BigDecimal bd3 = new BigDecimal(randomTemp).setScale(2, RoundingMode.CEILING);
         double randomAmbientTemp = new Random().nextDouble() * (39.5 - -15.3) + -15.3;
-        BigDecimal bd = new BigDecimal(randomAmbientTemp).setScale(2, RoundingMode.CEILING);
-        return bd.doubleValue();
+        BigDecimal bd4 = new BigDecimal(randomAmbientTemp).setScale(2, RoundingMode.CEILING);
+        double randomWindSpeed = new Random().nextDouble() * (10.3);
+        BigDecimal bd5 = new BigDecimal(randomWindSpeed).setScale(2, RoundingMode.CEILING);
+        Facility randomFacility = new Facility(recordID, timeStampID, date, bd1.doubleValue(), bd2.doubleValue(), bd3.doubleValue(), bd4.doubleValue(), bd2.doubleValue(), bd5.doubleValue());
+        return randomFacility;
     }
 
-    public double randomWindSpeed() {
-        double randomWindSpeed = new Random().nextDouble() * (10.3);
-        BigDecimal bd = new BigDecimal(randomWindSpeed).setScale(2, RoundingMode.CEILING);
-        return bd.doubleValue();
+    public PCC randomPCC() {
+        int tempRecord = 0;
+        tempRecord = new Random().nextInt() * 1000000;
+        recordID = Integer.toString(tempRecord);
+        TimeFactory timeFactory = new TimeFactory();
+        timeStampID = timeFactory.getCurrentID();
+        Date date = new Date();
+        double randomEnergy = new Random().nextDouble() * (6886468 - 6298630) + 6298630;
+        BigDecimal bd1 = new BigDecimal(randomEnergy).setScale(2, RoundingMode.CEILING);
+        double randomRealPower = new Random().nextDouble() * (6687 - -28) + -28;
+        BigDecimal bd2 = new BigDecimal(randomRealPower).setScale(2, RoundingMode.CEILING);
+        double randomApparantPower = new Random().nextDouble() * (7221 - -30.27) + -30.27;
+        BigDecimal bd3 = new BigDecimal(randomApparantPower).setScale(2, RoundingMode.CEILING);
+        double randomReactivePower = new Random().nextDouble() * (2726);
+        BigDecimal bd4 = new BigDecimal(randomReactivePower).setScale(2, RoundingMode.CEILING);
+        double randomPowerFactor = new Random().nextDouble() * (1 - 0.93) + 0.93;
+        BigDecimal bd5 = new BigDecimal(randomPowerFactor).setScale(2, RoundingMode.CEILING);
+        double randomPhaseCurrent = new Random().nextDouble() * (166 - -0.7) + -0.7;
+        BigDecimal bd6 = new BigDecimal(randomPhaseCurrent).setScale(2, RoundingMode.CEILING);
+        double randomPhaseVoltage = new Random().nextDouble() * (25225 - 25100) + 25100;
+        BigDecimal bd7 = new BigDecimal(randomPhaseVoltage).setScale(2, RoundingMode.CEILING);
+        PCC randomPCC = new PCC(recordID, timeStampID, date, bd1.doubleValue(), bd2.doubleValue(), bd3.doubleValue(), bd4.doubleValue(), bd5.doubleValue(), bd6.doubleValue(), bd6.doubleValue(), bd6.doubleValue(), bd7.doubleValue(), bd7.doubleValue(), bd7.doubleValue());
+        return randomPCC;
+    }
+
+    public Inverter randomInverter() {
+        int tempRecord = 0;
+        tempRecord = new Random().nextInt() * 1000000;
+        recordID = Integer.toString(tempRecord);
+        TimeFactory timeFactory = new TimeFactory();
+        timeStampID = timeFactory.getCurrentID();
+        Date date = new Date();
+        Integer deviceID = new Random().nextInt(39 - 1) + 1;
+        double randomOutputEnergy = new Random().nextDouble() * (6314779 - 6298630) + 6298630;
+        BigDecimal bd1 = new BigDecimal(randomOutputEnergy).setScale(2, RoundingMode.CEILING);
+        double randomRealPower = new Random().nextDouble() * (186);
+        BigDecimal bd2 = new BigDecimal(randomRealPower).setScale(2, RoundingMode.CEILING);
+        double randomApparantPower = new Random().nextDouble() * (138);
+        BigDecimal bd3 = new BigDecimal(randomApparantPower).setScale(2, RoundingMode.CEILING);
+        double randomPowerFactor = new Random().nextDouble() * (1);
+        BigDecimal bd4 = new BigDecimal(randomPowerFactor).setScale(2, RoundingMode.CEILING);
+        double randomPhaseCurrent = new Random().nextDouble() * (187);
+        BigDecimal bd5 = new BigDecimal(randomPhaseCurrent).setScale(2, RoundingMode.CEILING);
+        double randomPhaseVoltage = new Random().nextDouble() * (607);
+        BigDecimal bd6 = new BigDecimal(randomPhaseVoltage).setScale(2, RoundingMode.CEILING);
+        double randomDcReal = new Random().nextDouble() * (190);
+        BigDecimal bd7 = new BigDecimal(randomDcReal).setScale(2, RoundingMode.CEILING);
+        double randomDcVolt = new Random().nextDouble() * (1023);
+        BigDecimal bd8 = new BigDecimal(randomDcVolt).setScale(2, RoundingMode.CEILING);
+        double randomDcCurrent = new Random().nextDouble() * 186;
+        BigDecimal bd9 = new BigDecimal(randomDcCurrent).setScale(2, RoundingMode.CEILING);
+        double randomEfficency = new Random().nextDouble() * 1;
+        BigDecimal bd10 = new BigDecimal(randomEfficency).setScale(2, RoundingMode.CEILING);
+        double randomReactivePower = new Random().nextDouble() * 61;
+        BigDecimal bd11 = new BigDecimal(randomReactivePower).setScale(2, RoundingMode.CEILING);
+        Inverter randomInverter = new Inverter(recordID, timeStampID, date, deviceID, bd1.doubleValue(), bd2.doubleValue(), bd3.doubleValue(), bd11.doubleValue(), bd4.doubleValue(), bd5.doubleValue(), bd5.doubleValue(), bd5.doubleValue(), bd6.doubleValue(), bd6.doubleValue(), bd6.doubleValue(), bd7.doubleValue(), bd8.doubleValue(), bd9.doubleValue(), bd10.doubleValue());
+        return randomInverter;
+    }
+    
+    public Feeder randomFeeder() {
+                int tempRecord = 0;
+        tempRecord = new Random().nextInt() * 1000000;
+        recordID = Integer.toString(tempRecord);
+        TimeFactory timeFactory = new TimeFactory();
+        timeStampID = timeFactory.getCurrentID();
+        Date date = new Date();
+        Integer deviceID = new Random().nextInt(2-1)+1;
+        boolean randomBreakerStatus = new Random().nextBoolean();
+        double randomRealPower = new Random().nextDouble() * 3429;
+        BigDecimal bd1 = new BigDecimal(randomRealPower).setScale(2, RoundingMode.CEILING);
+        double randomApparantPower = new Random().nextDouble() * 3707;
+        BigDecimal bd2 = new BigDecimal(randomApparantPower).setScale(2, RoundingMode.CEILING);
+        double randomReactivePower = new Random().nextDouble() * 1409;
+        BigDecimal bd3 = new BigDecimal(randomReactivePower).setScale(2, RoundingMode.CEILING);
+        double randomPowerFactor = new Random().nextDouble() *(1 - 0.93) + 0.93;
+        BigDecimal bd4 = new BigDecimal(randomPowerFactor).setScale(2, RoundingMode.CEILING);
+        double randomPhaseCurrent = new Random().nextDouble() * 85;
+        BigDecimal bd5 = new BigDecimal(randomPhaseCurrent).setScale(2, RoundingMode.CEILING);
+        double randomPhaseVoltage = new Random().nextDouble() * 25225;
+        BigDecimal bd6 = new BigDecimal(randomPhaseVoltage).setScale(2, RoundingMode.CEILING);
+        if(randomBreakerStatus == true) {
+            Feeder randomFeeder = new Feeder(recordID, timeStampID, date, deviceID, randomBreakerStatus, bd1.doubleValue(),bd2.doubleValue(),bd3.doubleValue(),bd4.doubleValue(),bd5.doubleValue(), bd5.doubleValue(), bd5.doubleValue(),bd6.doubleValue(),bd6.doubleValue(),bd6.doubleValue());
+            return randomFeeder;
+        }
+        else {
+           Feeder downFeeder = new Feeder(recordID, timeStampID, date, deviceID, randomBreakerStatus,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
+           return downFeeder;
+        }
     }
 
 }

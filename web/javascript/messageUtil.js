f0b7d140929e36251710aeae8a2a0f636650ee5d
856622 <856622@SAIT227036IT.ACDM.DS.SAIT.CA>
2022-04-14 11:20:48 -0600

    Added message for reports page

--- web/javascript/messageUtil.js
+++ web/javascript/messageUtil.js
@@ -10,3 +10,14 @@ function errorAlert(msg) {
     });
 }
 
+function successAlert(msg) {
+    $(".bootstrap-growl").remove();
+    $.bootstrapGrowl(msg, {
+        type: 'info', // (null, 'info', 'error', 'success')
+        align: 'center', // ('left', 'right', or 'center')
+        width: 'auto', // (integer, or 'auto')
+        delay: 10000,
+        allow_dismiss: false,
+        stackup_spacing: 0 // spacing between consecutively stacked growls.
+    });
+}
